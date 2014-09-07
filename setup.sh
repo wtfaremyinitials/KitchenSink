0d2b6fc360348a14e51d7e7acca1ff2ee096a288
MC42 <tjflynn@theflynn.org>
2014-09-07 11:35:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash/
 
-read -p "Choose yor type of installation. : " type
+read -p "Choose your type of installation. : " type
          if $type = 1
-                  then 
+                  then echo "You have chosen an automated installation, including the best plugins the community has to offer."
                   
          if $type = 2
-                  then
+                  then echo "You have chosen a manual installation.  Please have your package codes ready to be entered."
                   
          else
                   exit
