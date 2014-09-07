8a2eb80fde15e733a0d81dd53f862a7b74b3a897
Will Franzen <wtfranzen@gmail.com>
2014-09-07 10:58:15 -0500

    Start router set up

--- client/app.js
+++ client/app.js
@@ -1,3 +1,8 @@
 Handlebars.registerHelper('signedIn', function() {
     return !!Meteor.userId();
 });
+
+Router.map(function() {
+    this.route('home',      { path: '/' });
+    this.route('signin',    { path: '/signin' });
+});
