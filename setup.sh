1a1f3d6757c33f74454c53bd393241fe09d5440a
MC42 <tjflynn@theflynn.org>
2014-09-07 11:33:04 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,11 @@
 #!/bin/bash/
 
-cmd=(dialog --separate-output --checklist "Select options:" 22 76 16)
-options=(1 "Option 1" off    # any option can be set to default to "on"
-         2 "Option 2" off
-         3 "Option 3" off
-         4 "Option 4" off)
-choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-clear
-for choice in $choices
-do
-    case $choice in
-        1)
-            echo "Auto Install"
-            ;;
-        2)
-            echo "Manual Installer"
-            ;;
-    esac
-done
+read -p "Choose yor type of installation. : " type
+         if $type = 1
+                  then 
+                  
+         if $type = 2
+                  then
+                  
+         else
+                  exit
