cd2ff483ef4c58cc9659b155d05e63bf06e8ff46
MC42 <tjflynn@theflynn.org>
2014-09-07 11:42:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 whineandbitch="Please make sure you are entering the correct type of installation."
 
 read -p "Choose your type of installation. : " type
-         if $type = 1
-                  then echo "You have chosen an automated installation, including the best plugins the community has to offer."
-                  else echo $whineandbitch
-         if $type = 2
-                  then echo "You have chosen a manual installation.  Please have your package codes ready to be entered."
-                  else echo $whineandbitch
+if $type = 1
+         then echo "You have chosen an automated installation, including the best plugins the community has to offer."
+         else echo $whineandbitch
+if $type = 2
+         then echo "You have chosen a manual installation.  Please have your package codes ready to be entered."
+         else echo $whineandbitch
+fi
